8bbeda6dd757e2ee1fb0a3d8c31b5dc8313439e4
DeanM312 <103493959+DeanM312@users.noreply.github.com>
2025-01-08 07:55:08 +1100

    Tic Tac Toe AI and Boids AI

---
+++ 10 - Lab - Steering 3/main.py
@@ -0,0 +1,96 @@
+'''Autonomous Agent Movement: Paths and Wandering
+
+Created for COS30002 AI for Games by Clinton Woodward <cwoodward@swin.edu.au>
+
+For class use only. Do not publically share or post this code without permission.
+
+This code is essentially the same as the base for the previous steering lab
+but with additional code to support this lab.
+
+'''
+from graphics import egi, KEY
+from pyglet import window, clock
+from pyglet.gl import *
+
+from vector2d import Vector2D
+from world import World
+from agent import Agent, AGENT_MODES  # Agent with seek, arrive, flee and pursuit
+from random import random, randrange, uniform
+
+def on_mouse_press(x, y, button, modifiers):
+    if button == 1:  # left
+        world.target = Vector2D(x, y)
+
+
+def on_key_press(symbol, modifiers):
+    if symbol == KEY.P:
+        world.paused = not world.paused
+    elif symbol in AGENT_MODES:
+        for agent in world.agents:
+            agent.mode = AGENT_MODES[symbol]
+    elif symbol == KEY.L:
+        world.agents.append(Agent(world))
+    elif symbol == KEY.R:
+        for a in world.agents:
+            a.randomise_path()
+       
+
+    # Toggle debug force line info on the agent
+    elif symbol == KEY.I:
+        for agent in world.agents:
+            agent.show_info = not agent.show_info
+
+
+
+def on_resize(cx, cy):
+    world.cx = cx
+    world.cy = cy
+
+
+if __name__ == '__main__':
+
+    # create a pyglet window and set glOptions
+    win = window.Window(width=1000, height=1000, vsync=True, resizable=True)
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    # needed so that egi knows where to draw
+    egi.InitWithPyglet(win)
+    # prep the fps display
+    fps_display = window.FPSDisplay(win)
+    # register key and mouse event handlers
+    win.push_handlers(on_key_press)
+    win.push_handlers(on_mouse_press)
+    win.push_handlers(on_resize)
+
+    # create a world for agents
+    world = World(1000, 1000)
+    
+    hunter = Agent(world)
+    hunter.mode = 'wander'
+    hunter.color = 'RED'
+    prey = Agent(world)
+    world.agents.append(hunter)
+    world.agents.append(prey)
+
+    world.hunter = hunter
+
+    for i in range(20):
+        pos = Vector2D(randrange(world.cx), randrange(world.cy))
+        world.rocks.append(pos)
+
+
+    
+    # unpause the world ready for movement
+    world.paused = False
+
+    while not win.has_exit:
+        win.dispatch_events()
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        # show nice FPS bottom right (default)
+        delta = clock.tick()
+        world.update(delta)
+        world.render()
+        fps_display.draw()
+        # swap the double buffer
+        win.flip()
+
